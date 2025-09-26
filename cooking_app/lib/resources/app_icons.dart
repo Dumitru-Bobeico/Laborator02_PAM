729b7e9aa5b7b90d5e649ea08348db8b5b5f3ff2
Dumitru-Bobeico <dima.bobeico.2003@gmail.com>
2025-09-26 07:50:56 +0300

    boilerplate for app_icons and app_strings

---
+++ cooking_app/lib/resources/app_icons.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppIcons {
+  static const String user = "assets/icons/user.png";
+  static const String home = "assets/icons/home.png";
+  static const String settings = "assets/icons/settings.png";
+
+  static const IconData add = Icons.add;
+  static const IconData edit = Icons.edit;
+  static const IconData delete = Icons.delete;
+}
---
+++ cooking_app/lib/resources/app_strings.dart
@@ -0,0 +1,15 @@
+class AppStrings {
+  static const String login = "Login";
+  static const String signup = "Sign Up";
+  static const String email = "Email";
+  static const String password = "Password";
+
+  static const String errorGeneral = "Something went wrong. Please try again.";
+  static const String errorInvalidEmail = "Invalid email format";
+  static const String errorEmptyField = "This field cannot be empty";
+
+  static const String ok = "OK";
+  static const String cancel = "Cancel";
+  static const String settings = "Settings";
+  static const String profile = "Profile";
+}
